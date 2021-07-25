4838cc3c6e3db27dc3ae9b8930a99c6a86c026a3
Tuanh4198 <nguyenanh.it.4198@gmail.com>
2021-07-25 12:49:05 +0700

    build folder structure

---
+++ javascript/main.js
@@ -0,0 +1,77 @@
+jQuery(document).ready(function($) {
+	console.log('object')
+
+	$(".autoplay").each(function() {
+		$(this).slick($(this).data());
+	});
+
+	// fixed menu
+	(function ($) {
+		let	menu = $('.fixed-main-menu');
+			body = $('body,html');
+			menuPosition = menu.offset().top;
+		$(window).scroll( () => {
+			let startpage = body.scrollTop();
+			(startpage > menuPosition) ? (menu.addClass('fixed')) : (menu.removeClass('fixed'))
+		});	
+	})($);
+	// end fixed menu
+
+	// load top
+	(function ($) {
+		let up_btn = $("body .up");
+		let body = $('body,html');
+		up_btn.css({
+			cursor: 'pointer'
+		});
+		up_btn.click(function() {
+			$('html,body').animate({scrollTop: 0}, 1000);
+		});
+		$(window).scroll(function(event){
+			let startpage = body.scrollTop();
+			if(startpage > 200)
+			{
+				up_btn.addClass('up-active');		
+			}
+			else if(startpage < 200){
+				up_btn.removeClass('up-active');	
+			}
+		});
+	})($);
+	// end load top
+
+	// Effect accordion
+	$(function() {
+		$('.accordion .show-option').click(function(event) {
+			event.preventDefault();
+			$(this).parent().siblings().find('.fretboard').slideUp();
+			$(this).parent().find('.fretboard').slideToggle();
+
+			$(this).parent().siblings().find('.show').removeClass('active').text('+');
+			($(this).parent().find('.show').text() == '-') ? ($(this).parent().find('.show').removeClass('active').text('+')) : ($(this).parent().find('.show').addClass('active').text('-'))
+
+			$(this).parent().find('i').toggleClass('rotate');
+			$(this).parent().siblings().find('i').removeClass('rotate');
+		});
+	});
+	// end Effect accordion
+
+	// Effect drop down
+	(function ($) {
+		let box = $('body .inside');	
+		box.find('.drop-down').slideUp();
+		$(document).mouseup(e => {
+		    if (!box.is(e.target) && box.has(e.target).length === 0) 
+		    { 
+		    	box.find('.drop-down').slideUp(); 
+		    }
+		});
+		box.find('.command-button').on('click', function(event) {
+			event.preventDefault();
+			$(this).parent().siblings().find('.drop-down').slideUp();
+			$(this).parent().find('.drop-down').slideToggle();
+		});
+	})($);
+	// end Effect drop down
+
+});
